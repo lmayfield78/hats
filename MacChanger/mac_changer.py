a932ba1ba071baaaef73ee8b8fe1809aec3e5360
Lawrence <lawrence@ITs-MacBook-Pro.local>
2019-03-17 00:37:14 -0500

    Update

--- MacChanger/mac_changer.py
+++ MacChanger/mac_changer.py
@@ -7,7 +7,7 @@ new_mac = "00:11:33:55:77:99"
 
 
 
-subprocess.call("ifconfig" + interface + "down", shell=True)
-subprocess.call("ifconfig" + interface + "hw ether" + new_mac, shell=True)
-subprocess.call("ifconfig" + interface + "up", shell=True)
+subprocess.call("ifconfig " + interface + " down", shell=True)
+subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
+subprocess.call("ifconfig " + interface + " up", shell=True)
 
